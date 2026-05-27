dd4fd8df6b875d95310e0d0d52e55758f1896f36
Pandu-Hub12 <scriptbahan@gmail.com>
2026-05-27 19:14:46 +0700

    Library.lua

--- Library.lua
+++ Library.lua
@@ -224,7 +224,7 @@ local Library = {
         AccentColor = Color3.fromRGB(125, 85, 255),
         OutlineColor = Color3.fromRGB(40, 40, 40),
         FontColor = Color3.new(1, 1, 1),
-        Font = Font.fromEnum(Enum.Font.Code),
+        Font = Font.fromEnum(Enum.Font.Roboto),
 
         RedColor = Color3.fromRGB(255, 50, 50),
         DestructiveColor = Color3.fromRGB(220, 38, 38),
@@ -317,7 +317,7 @@ local Templates = {
         CornerRadius = 4,
         NotifySide = "Right",
         ShowCustomCursor = true,
-        Font = Enum.Font.Code,
+        Font = Enum.Font.Roboto,
         ToggleKeybind = Enum.KeyCode.RightControl,
         
         ShowMobileButtons = true,
