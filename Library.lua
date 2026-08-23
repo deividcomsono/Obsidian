c02e6b1a8cf2549552505bbe9ab8bdbd5022b320
corrade <141366848+corradedied@users.noreply.github.com>
2026-08-23 06:51:09 -0400

    Disconnect PriorityIndicatorConnection if it exists

--- Library.lua
+++ Library.lua
@@ -9086,6 +9086,11 @@ function Library:Notify(...)
             DeleteConnection:Disconnect()
         end
 
+        if PriorityIndicatorConnection then
+            PriorityIndicatorConnection:Disconnect()
+            PriorityIndicatorConnection = nil
+        end
+
         if FakeBackground then
             local Idx = table.find(NotifyOrder, FakeBackground)
             if Idx then
